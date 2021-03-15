874aa3abc019c202c646beb985707510630d8c7d
Canberk Ardıç <canberk.ardic@cantekgroup.com>
2021-03-15 22:43:18 +0300

    Project Set-up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CandidatesComponent } from './candidates/candidates.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'home',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  {
+    path: 'candidates',
+    component: CandidatesComponent,
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent
+  },
+  {
+    path : '**',
+    component : HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CandidatesComponent } from './candidates/candidates.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CandidateDetailComponent } from './candidate-detail/candidate-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CandidatesComponent,
+    AboutUsComponent,
+    CandidateDetailComponent
   ],
   imports: [
     BrowserModule,
